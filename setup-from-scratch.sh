eb730a9fe116d1303066e3943704af9c9cfce4c0
Chih-Lung Yeh <alanclyeh@gmail.com>
2017-03-08 23:37:56 +0800

    Add scritp to install oh-my-zsh and add custom configuration

--- setup-from-scratch.sh
+++ setup-from-scratch.sh
@@ -10,6 +10,20 @@ brew install $(cat brew-packages)
 brew tap caskroom/cask
 brew cask install $(cat brew-cask-packages)
 
+# Install oh-my-zsh
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+# Backup original .zshrc 
+cp ~/.zshrc ~/.zshrc.orig
+
+# *** Custom ZSH Configuration *** #
+# theme
+sed -i "s/^ZSH_THEME=.*/ZSH_THEME="yc"/" ~/.zshrc
+
+# plugins
+sed -i "s/^plugins=(git.*/plugins=(git osx vagrant docker)/" ~/.zshrc
+# ******************************* #
+
 echo
 echo "DONE!!"
 echo
